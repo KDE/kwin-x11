0fdb4808e566bf503213e103e4b6537dc7b5ffa5
Rémi Bernon <rbernon@codeweavers.com>
2025-03-20 14:52:30 +0100

    x11window: Remove unnecessary condition.
    isMaximizable already checks for the same conditions.
    
    (cherry picked from commit 51197c5088ab3560c3f87a488986bb9260b3a95f)

--- src/x11window.cpp
+++ src/x11window.cpp
@@ -4540,9 +4540,6 @@ void X11Window::maximize(MaximizeMode mode, const QRectF &restore)
         return;
     }
 
-    if (!isResizable() || isToolbar()) { // SELI isToolbar() ?
-        return;
-    }
     if (!isMaximizable()) {
         return;
     }
